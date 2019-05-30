f7f6cd49839bdc11e4df29f4c77fbfd7c40a4864
Jack "Conrad" Clark <9323501+clarkmyfancy@users.noreply.github.com>
2019-05-30 00:27:11 -0500

    made user input consistent across program

--- Console.py
+++ Console.py
@@ -11,24 +11,34 @@ class Console:
 	def promptStartSelectionToUser(self):
 		print("Enter \"A\" to add a task to the todo list.")
 		print("Enter \"D\" to display the current todo list.")
-		print("Enter \"P\" to view prioritized todo list.")
+		print("Enter \"C\" to enter section to mark a task as completed.")
+		# print("Enter \"R\" to remove a task from the todo list.")
+		print("--> ", end="")
 		userSelection = input()
 		print()
 		return userSelection
 
-		# user input is always scary, this should have error correction
 	def getNewTaskDataFromUser(self):
-		name = input("Enter the name of the task: ")
-		type = input("Enter the type of the task: ")
-		priority = input("Enter the priority of the task: ")
-		finishByDateTime = input("Enter the completion date with format \"yyyy-dd-mm hh:mm:ss\": ")
-			# if no time given could assume the time is like 7pm or something
-		descr = input("Enter the task's description: ")
+		print("Enter the name of the task.")
+		print("--> ", end="")
+		name = input()
+		print("Enter the type of the task.")
+		print("--> ", end="")
+		type = input()
+		print("Enter the priority of the task.")
+		print("--> ", end="")
+		priority = input()
+		print("Enter the completion date of the task with the time.")
+		print("--> ", end="")
+		finishByDateTime = input()
+		print("Enter the task's description.")
+		print("--> ", end="")
+		descr = input()
 		print()
 		return (name, type, priority, finishByDateTime, descr)
 
 	def displayTodoListWithFieldsAndRecords(self, fields, records):
-		print(fields[0].upper() + "\t\t| " + fields[5].upper())
+		print("TASK" + "\t\t| " + fields[5].upper())
 		dashes = ""
 		for x in range(0, 100):
 			dashes += "-"
