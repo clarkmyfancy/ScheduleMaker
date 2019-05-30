ba9277c4cd0a5648538cb49154c459689c2af6b3
Jack "Conrad" Clark <9323501+clarkmyfancy@users.noreply.github.com>
2019-05-30 00:28:57 -0500

    using taskDAO for com. and inc. tasks (incomplete)

--- TaskDAO.py
+++ TaskDAO.py
@@ -14,26 +14,36 @@ class TaskDAO():
 		self.scope = ['https://www.googleapis.com/auth/drive']
 		self.serviceAccountCreds = SAC.from_json_keyfile_name('Scheduler-30eefa3a4c69.json', self.scope)
 		self.gc = gspread.authorize(self.serviceAccountCreds)
-		self.worksheet = self.gc.open('taskDB').sheet1
-		self.numTasks = len(self.worksheet.col_values(1)) - 1
+		self.worksheet = self.gc.open('taskDB')
+		self.incompleteTasksWorksheet = self.worksheet.worksheet('Incomplete Tasks')
+		# self.incompleteTasksWorksheet = self.gc.open('taskDB').sheet1
+		# self.completeTasksWorksheet = self.gc.open('taskDB').sheet2
+		self.numIncompleteTasks = len(self.incompleteTasksWorksheet.col_values(1)) - 1
+		# self.numCompleteTasks = len(self.completeTasksWorksheet.col_values(1)) - 1
 
 	def updateDBWithTask(self, newTask):
-		firstEmptyRow = str(self.numTasks + 2)
-		self.worksheet.update_acell('A' + firstEmptyRow, newTask.name)
-		self.worksheet.update_acell('B' + firstEmptyRow, numTasks)
-		self.worksheet.update_acell('C' + firstEmptyRow, newTask.type)
-		self.worksheet.update_acell('D' + firstEmptyRow, newTask.priority)
-		self.worksheet.update_acell('E' + firstEmptyRow, newTask.finishByDateTime)
-		self.worksheet.update_acell('F' + firstEmptyRow, newTask.description)
+		firstEmptyRow = str(self.numIncompleteTasks + 2)
+		self.incompleteTasksWorksheet.update_acell('A' + firstEmptyRow, newTask.name)
+		self.incompleteTasksWorksheet.update_acell('B' + firstEmptyRow, self.numIncompleteTasks)
+		self.incompleteTasksWorksheet.update_acell('C' + firstEmptyRow, newTask.type)
+		self.incompleteTasksWorksheet.update_acell('D' + firstEmptyRow, newTask.priority)
+		self.incompleteTasksWorksheet.update_acell('E' + firstEmptyRow, newTask.finishByDateTime)
+		self.incompleteTasksWorksheet.update_acell('F' + firstEmptyRow, newTask.description)
 		# possible bug here when the db gets updated but the numTasks is
 			# not updated, will cause inconsitencies and that is rly bad
-		self.numTasks += 1
+		self.numIncompleteTasks += 1
 
 	def getFieldsFromDB(self):
-		return self.worksheet.row_values(1)
+		return self.incompleteTasksWorksheet.row_values(1)
 
 	def getRecordsFromDB(self):
 		ls = []
-		for i in range(2, self.numTasks+2):
-			ls.append(self.worksheet.row_values(i))
+		for i in range(2, self.numIncompleteTasks+2):
+			ls.append(self.incompleteTasksWorksheet.row_values(i))
 		return ls
+
+	def markTaskCompletedWithId(self, taskId):
+		# copy the data in the specified row of the first sheet
+		# use that data to create some sort of object to hold it?
+		# then insert the data into the sheet containing completed tasks
+		pass
